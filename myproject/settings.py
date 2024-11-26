fd793222e51e6875d021c132f4549ef9d50e8626
Ridwan Oyenuga <taonuga01@gmail.com>
2024-11-26 10:32:28 +0100

    updated cors

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,7 +78,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # settings.py
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',  # Frontend on local machine
+    'http://127.0.0.1:5500',  
 ]
 
 ALLOWED_HOSTS = ['*']
