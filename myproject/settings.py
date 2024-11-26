70af6bb070ff33fece5ee861e6a5512802808b44
Ridwan Oyenuga <taonuga01@gmail.com>
2024-11-26 11:56:17 +0100

    updated cors

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,7 +78,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # settings.py
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',  
+    'http://127.0.0.1:5500', 
+    'http://127.0.0.1:3000', 
+    'https://kaygeedev.github.io/LoanManagementSystem/', 
 ]
 
 ALLOWED_HOSTS = ['*']
